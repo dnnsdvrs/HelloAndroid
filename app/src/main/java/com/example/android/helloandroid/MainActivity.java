1fc5687ff296953ed46b9567d0f0f10c0b4835a2
DennisdeVries <dnnsdvrs@gmail.com>
2017-11-22 13:51:48 +0100

    style: shorter code and clickable areas
    By using drawableLeft I was able to delete imageviews and the surrounding viewgroup, resulting in a much shorter code. Alse all areas and social media icons are now clickable.

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.helloandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void facebookButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Udacity/"));
+        startActivity(browserIntent);
+
+    }
+
+    public void githubButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/udacity"));
+        startActivity(browserIntent);
+
+    }
+
+    public void twitterButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/udacity"));
+        startActivity(browserIntent);
+
+    }
+
+    public void youtubeButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCBVCi5JbYmfG3q5MEuoWdOw"));
+        startActivity(browserIntent);
+
+    }
+
+    public void addressButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=udacity"));
+        startActivity(browserIntent);
+
+    }
+
+    public void phoneButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:6505555555"));
+        startActivity(browserIntent);
+
+    }
+
+    public void webButton(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.udacity.com"));
+        startActivity(browserIntent);
+
     }
 }
